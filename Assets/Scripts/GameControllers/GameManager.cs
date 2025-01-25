c7e58b7795d1a0d2dee93a75cd7fadbe9f9f675f
Christiano Walter <125219293+ChristianoWalter@users.noreply.github.com>
2025-01-25 18:06:33 -0300

    Wireframes Update

---
+++ Assets/Scripts/GameControllers/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Main Screens")]
+    [SerializeField] GameObject workareaScreen;
+    [SerializeField] GameObject feedScreen;
+
+    [Header("Pop Ups Screens")]
+    [SerializeField] GameObject generalPopUpScreen;
+    [SerializeField] GameObject postPopUp;
+    [SerializeField] GameObject settingsPopUp;
+    [SerializeField] GameObject notesPopUp;
+    [SerializeField] GameObject errorPopUp;
+
+    public void SwitchMainScreens(int index)
+    {
+        workareaScreen.SetActive(index == 1);
+        feedScreen.SetActive(index == 2);
+    }
+
+    public void SwitchPopUp(int index)
+    {
+        generalPopUpScreen.SetActive(index != 0);
+        settingsPopUp.SetActive(index == 1);
+        notesPopUp.SetActive(index == 2);
+        errorPopUp.SetActive(index == 3);
+        postPopUp.SetActive(index == 4);
+    }
+}
