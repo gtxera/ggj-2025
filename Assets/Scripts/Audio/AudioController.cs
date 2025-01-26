a53a5f6774cdab51cff3d36f553fccf2078bcb57
txera <gabrieltxera@gmail.com>
2025-01-26 15:47:25 -0300

    adiciona sliders de volume

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -7,6 +7,9 @@ public class AudioController : MonoBehaviour
 {
     [SerializeField]
     private AudioMixer _mixer;
+    
+    [SerializeField]
+    private Slider _masterSlider;
 
     [SerializeField]
     private Slider _sfxSlider;
@@ -16,6 +19,8 @@ public class AudioController : MonoBehaviour
 
     private void Start()
     {
-        _sfxSlider.onValueChanged.AddListener((f) => _mixer.SetFloat("SFXVolume", f));
+        _sfxSlider.onValueChanged.AddListener((f) => _mixer.SetFloat("SFXVolume", Mathf.Log10(f) * 20));
+        _musicSlider.onValueChanged.AddListener((f) => _mixer.SetFloat("MusicVolume", Mathf.Log10(f) * 20));
+        _masterSlider.onValueChanged.AddListener((f) => _mixer.SetFloat("MasterVolume", Mathf.Log10(f) * 20));
     }
 }
